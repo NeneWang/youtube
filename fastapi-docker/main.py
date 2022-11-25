e1dde1a865cc79a91961f3dcfc9d1970f739892e
NelsonWang <wangnelson2@gmail.com>
2022-11-25 09:27:42 -0500

    Update main.py

--- fastapi-docker/main.py
+++ fastapi-docker/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return { "message": "hello world This is the updated version"}
+    return { "message": "hello world This is the Another update version"}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host='0.0.0.0')
